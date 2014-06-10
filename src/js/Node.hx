a01b69712e10efb2c4032d740dd81170f8ba5441
Nicolas Cannasse <ncannasse@gmail.com>
2014-06-10 10:06:17 +0200

    removed Error (duplicated from js.Error)

--- src/js/Node.hx
+++ src/js/Node.hx
@@ -848,12 +848,6 @@ typedef NodeZLib = {
 	function unzip(buf:NodeBuffer,cb:NodeErr->Dynamic->Void):Void;
 }
 
-@:native("Error")
-extern class Error
-{
-	public function new(msg : String) : Void;
-}
-
 // Node Constants
 class NodeC {
 	public static inline var UTF8 = "utf8";
